357465c6048d241ba2e7a15aaceaf84a2f8f6f6c
Dennis Koot <koter84@gmail.com>
2021-12-01 12:09:19 +0100

    device options

--- ebusd/CHANGELOG.md
+++ ebusd/CHANGELOG.md
@@ -1,5 +1,9 @@
 <!-- https://developers.home-assistant.io/docs/add-ons/presentation#keeping-a-changelog -->
 
+## 0.82
+- IMPROVE: add possibility to set custom device (for networked devices)
+- IMPROVE: setup ports for eBUSd and http
+
 ## 0.81
 
 - FIX: null values were not accepted for some options
--- ebusd/run.sh
+++ ebusd/run.sh
@@ -2,8 +2,8 @@
 
 declare -a ebusd_args
 
-#boolean options
-declare options=( "readonly" "scanconfig" "foreground" "mqttjson" "mqttlog" "mqttretain")
+# boolean options
+declare options=( "readonly" "scanconfig" "foreground" "mqttjson" "mqttlog" "mqttretain" )
 for optName in "${options[@]}"
 do
     if bashio::config.true ${optName}; then
@@ -11,14 +11,33 @@ do
     fi
 done
 
-#other options
-declare options=( "device" "port" "latency" "logareas" "loglevel" "mqtthost" "mqttport" "mqttuser" "mqttpass")
+# other options
+declare options=( "latency" "logareas" "loglevel" "mqtthost" "mqttport" "mqttuser" "mqttpass" )
 for optName in "${options[@]}"
 do
     if ! bashio::config.is_empty ${optName}; then
         ebusd_args+=("--${optName}=$(bashio::config ${optName})")
     fi
 done
+
+# device options
+if ! bashio::config.is_empty 'device_usb'; then
+    ebusd_args+=("--device=$(bashio::config 'device_usb')")
+elif ! bashio::config.is_empty 'device_custom'; then
+    ebusd_args+=("--device=$(bashio::config 'device_custom')")
+else
+	bashio::log.fatal "either device_usb or device_custom needs to be set"
+	exit
+fi
+
+# port options
+ebusd_args+=("--port=8888")
+ebusd_args+=("--httpport=8889")
+
+# uhm, this doesn't work, since that would be the external port...
+#ebusd_args+=("--port=$(bashio::addon.port '8888/tcp')")
+#ebusd_args+=("--httpport=$(bashio::addon.port '8889/tcp')")
+
 
 if bashio::config.false "foreground" || bashio::config.is_empty "foreground"; then
     bashio::config.suggest.true "foreground" "ebusd add-on will stop if ebusd is not running in the foreground."
